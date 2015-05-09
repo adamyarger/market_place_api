806a4bb671851298cb7428016fbb1aa84ce5c5a7
adamyarger <yargeradam@gmail.com>
2015-05-09 10:59:46 -0500

    adds update action to users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -17,6 +17,7 @@ describe Api::V1::UsersController do
     it { should respond_with 200 }
   end
 
+
   describe "POST #create" do
 
     context "when is successfully created" do
@@ -54,7 +55,43 @@ describe Api::V1::UsersController do
       it { should respond_with 422 }
     end
   end
-  
+
+
+  describe "PUT/PATCH #update" do
+     context "when is succesfully updated" do
+       before(:each) do
+         @user = FactoryGirl.create :user
+         patch :update, { id: @user.id, user: { email: "newmail@example.com"}}, format: :json
+       end
+
+       it "renders the json representation for the updated user" do
+         user_response = JSON.parse(response.body, symbolize_names: true)
+         expect(user_response[:email]).to eql "newmail@example.com"
+       end
+
+       it { should respond_with 200 }
+     end
+
+     context "when is not created" do
+       before(:each) do
+         @user = FactoryGirl.create :user
+         patch :update, { id: @user.id, user: { email: "badmail.com"}}, format: :json
+       end
+
+       it "renders an errors json" do
+         user_response = JSON.parse(response.body, symbolize_names: true)
+         expect(user_response).to have_key(:errors)
+       end
+
+       it "renders the json errors on why the user could not be created" do
+         user_response = JSON.parse(response.body, symbolize_names: true)
+         expect(user_response[:errors][:email]).to include "is invalid"
+       end
+
+       it { should respond_with 422 }
+     end
+  end
+
 end
 
 
