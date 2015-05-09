f9de7ebdf2fd67cc9201b4ac931ec563c103363e
adamyarger <yargeradam@gmail.com>
2015-05-09 10:19:40 -0500

    updates app controller to prevent csfr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,3 @@
-class Api::V1::UsersController < ApplicationController
-  respond_to :json
-
-  def show
-    respond_with User.find(params[:id])
-  end
+class UsersController < ApplicationController
+  
 end
