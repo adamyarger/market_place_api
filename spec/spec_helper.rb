03088fa3afe2bc5a84064cf010a5e93b58bf23e1
adamyarger <yargeradam@gmail.com>
2015-05-09 18:20:54 -0500

    headers are still working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,5 +42,10 @@ RSpec.configure do |config|
 
   config.warnings = false
 
-config.include Request::JsonHelpers, :type => :controller
+  config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
