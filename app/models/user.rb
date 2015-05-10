dab7e86826152a4df6ce309a7fff9092ad8a6844
adamyarger <yargeradam@gmail.com>
2015-05-10 12:53:13 -0500

    adds user product association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ActiveRecord::Base
 	validates :auth_token, uniqueness: true
 	before_create :generate_authentication_token!
 
+	has_many :products, dependent: :destroy
+
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 
     def generate_authentication_token!
     	begin
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,9 @@ describe User do
 	it { should respond_to(:auth_token) }
 	it { should validate_uniqueness_of(:auth_token) }
 
-	describe "#genegerate_authentication_token!" do
+	it { should have_many(:products) }
+
+	describe "#generate_authentication_token!" do
 		it "generates a unique token" do
 			Devise.stub(:friendly_token).and_return("auniquetoken123")
 			@user.generate_authentication_token!
@@ -24,6 +26,21 @@ describe User do
 			existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
 			@user.generate_authentication_token!
 			expect(@user.auth_token).not_to eql existing_user.auth_token
+		end
+	end
+
+	describe "#products association" do
+		before do
+			@user.save
+			3.times { FactoryGirl.create :product, user: @user }
+		end
+
+		it "destroys the association products on self destruct" do
+			products = @user.products
+			@user.destroy
+			products.each do |product|
+				expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+			end
 		end
 	end
 
