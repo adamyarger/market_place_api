8dc9da1f3efa56ef4b183c54ad180de886cfb3d8
adamyarger <yargeradam@gmail.com>
2015-05-10 14:12:48 -0500

    adds create action for products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,12 +1,28 @@
 class Api::V1::ProductsController < ApplicationController
 	respond_to :json
 
+	def create
+		product = current_user.products.build(product_params)
+
+		if product.save
+			render json: product, status: 201, location: [:api, product]
+		else
+			render json: { errors: product.errors }, status: 422
+		end
+	end
+
 	def show
 		respond_with Product.find(params[:id])
 	end
 
 	def index
 	    respond_with Product.all  
-	  end
+	end
+
+	private
+
+		def product_params
+			params.require(:product).permit(:title, :price, :published)
+		end
 
 end
