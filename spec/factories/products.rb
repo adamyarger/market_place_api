89dfbff00a4f7c7a154b09c01c37ac53ed920058
adamyarger <yargeradam@gmail.com>
2015-05-11 17:53:21 -0500

    allows the order to be placed along with product quantity

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
-	factory :product do
-		title { FFaker::Product.product_name }
-		price { rand() * 100 }
-		published false
-		user
-	end
+  factory :product do
+    title { FFaker::Product.product_name }
+    price { rand() * 100 }
+    published false
+    user
+    quantity 5 #this is the line we added
+  end
 end
