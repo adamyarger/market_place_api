3ee18404d6a33cfd9cf020681aea8248d3f64f72
adamyarger <yargeradam@gmail.com>
2015-05-11 18:47:20 -0500

    adds quantity to placements model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511223922) do
+ActiveRecord::Schema.define(version: 20150511234517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20150511223922) do
   create_table "placements", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 0
   end
 
   add_index "placements", ["order_id"], name: "index_placements_on_order_id", using: :btree
